72245900f5c26fcca7620ba79381f5a10118eb7f
Ran <ry_always@163.com>
2022-11-12 17:59:02 +0800

    closed shape and rect, oval

---
+++ extension/src/main/java/cn/extension/ext/shape/AbstractClosedShape.java
@@ -0,0 +1,111 @@
+package cn.extension.ext.shape;
+
+import cn.extension.exec.ParameterException;
+import cn.extension.ext.OverlayShape;
+import cn.extension.tool.GenericBuilder;
+
+import java.awt.*;
+
+/**
+ * 封闭图像
+ *
+ * @author tracy
+ * @since 1.0.0
+ */
+public abstract class AbstractClosedShape implements OverlayShape {
+
+    /**
+     * 绘制区域
+     */
+    protected Rectangle rect;
+
+    /**
+     * 笔画样式，例如 BasicStroke
+     */
+    protected Stroke stroke;
+
+    /**
+     * 是否填充
+     */
+    protected boolean fill;
+
+    /**
+     * 颜色
+     */
+    protected Color color;
+
+
+    public AbstractClosedShape(ClosedShapeBuilder csb) {
+        this.rect = csb.rect;
+        this.stroke = csb.stroke;
+        this.fill = csb.fill;
+        this.color = csb.color;
+    }
+
+    @Override
+    public void paint(int canvasWidth, int canvasHeight, Graphics2D g2d) {
+        // check for rect is out of bounds
+        if (rect == null) {
+            throw new ParameterException("not specified any rect for this shape");
+        } else {
+            int maxX = rect.x + rect.width;
+            int maxY = rect.y + rect.height;
+            if (maxX > canvasWidth || maxY > canvasHeight) {
+                throw new ParameterException("the rect is out of image");
+            }
+        }
+
+        if (fill) {
+            // the default fill color is white
+            g2d.setColor(color == null ? Color.WHITE : color);
+            fillInside(canvasWidth, canvasHeight, g2d);
+        } else {
+            // the default draw color is black
+            g2d.setColor(color == null ? Color.BLACK : color);
+            // the default store is basic stroke
+            g2d.setStroke(stroke == null ? new BasicStroke() : stroke);
+            drawBorder(canvasWidth, canvasHeight, g2d);
+        }
+    }
+
+    /**
+     * 绘制边框
+     * @param canvasWidth 画笔宽
+     * @param canvasHeight 画笔高
+     * @param g2d 画笔
+     */
+    public abstract void drawBorder(int canvasWidth, int canvasHeight, Graphics2D g2d);
+
+    /**
+     * 填充内部
+     * @param canvasWidth 画笔宽
+     * @param canvasHeight 画笔高
+     * @param g2d 画笔
+     */
+    public abstract void fillInside(int canvasWidth, int canvasHeight, Graphics2D g2d);
+
+
+    public abstract static class ClosedShapeBuilder implements GenericBuilder<AbstractClosedShape> {
+        protected Rectangle rect;
+        protected Stroke stroke;
+        protected boolean fill;
+        protected Color color;
+
+        public ClosedShapeBuilder rect(Rectangle rect) {
+            this.rect = rect;
+            return this;
+        }
+        public ClosedShapeBuilder stroke(Stroke stroke){
+            this.stroke = stroke;
+            return this;
+        }
+        public ClosedShapeBuilder fill(boolean fill){
+            this.fill = fill;
+            return this;
+        }
+        public ClosedShapeBuilder color(Color color){
+            this.color = color;
+            return this;
+        }
+    }
+}
---
+++ extension/src/main/java/cn/extension/ext/shape/closed/Rect.java
@@ -0,0 +1,61 @@
+package cn.extension.ext.shape.closed;
+
+import cn.extension.exec.ParameterException;
+import cn.extension.ext.shape.AbstractClosedShape;
+
+import java.awt.*;
+
+/**
+ * 矩形
+ *
+ * @author tracy
+ * @since 1.0.0
+ */
+public class Rect extends AbstractClosedShape {
+
+    public Rect(Builder bu) {
+        super(bu);
+    }
+
+    @Override
+    public void drawBorder(int canvasWidth, int canvasHeight, Graphics2D g2d) {
+        g2d.drawRect(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    @Override
+    public void fillInside(int canvasWidth, int canvasHeight, Graphics2D g2d) {
+        g2d.fillRect(rect.x, rect.y, rect.width, rect.height);
+    }
+
+
+    public static class Builder extends ClosedShapeBuilder {
+
+        @Override
+        public Builder rect(Rectangle rect) {
+            return (Builder) super.rect(rect);
+        }
+
+        @Override
+        public Builder stroke(Stroke stroke) {
+            return (Builder) super.stroke(stroke);
+        }
+
+        @Override
+        public Builder fill(boolean fill) {
+            return (Builder) super.fill(fill);
+        }
+
+        @Override
+        public Builder color(Color color) {
+            return (Builder) super.color(color);
+        }
+
+        @Override
+        public Rect build() {
+            if (rect == null) {
+                throw new ParameterException("not specified any rect for this shape");
+            }
+            return new Rect(this);
+        }
+    }
+}
