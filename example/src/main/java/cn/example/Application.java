95a239eb9297037ac1f70321d9be2942fc3cd117
Ran <ry_always@163.com>
2022-11-15 20:41:47 +0800

    reconstruct project structure

--- example/src/main/java/cn/example/Application.java
+++ example/src/main/java/cn/example/Application.java
@@ -1,16 +1,13 @@
 package cn.example;
 
-import cn.core.wrapper.DefaultWrapper;
-import cn.extension.ext.split.GridSplitStrategy;
-import net.coobird.thumbnailator.Thumbnails;
+import cn.captor.gen.HashImageGenerator;
+import cn.captor.gen.MonoColorImageGenerator;
+import cn.core.utils.ColorUtils;
+import cn.usage.Captors;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
 
 /**
  * Application
@@ -22,46 +19,13 @@ public class Application {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException, AWTException {
         long l1 = System.currentTimeMillis();
 
-//        DefaultWrapper
-//                .from(new HashImageCaptor.Builder("my name")
-//                        .gridVerticalNum(8)
-//                        .fgColor(ColorUtils.of(200, 0, 200))
-//                        .build())
-//                .expand(new HighQualityExpandHandler.Builder()
-//                        .keepAspectRatio(true)
-//                        .finalHeight(240)
-//                        .build())
-//                .border(new BorderHandler.Builder()
-//                        .vMargins(20)
-//                        .fillColor(ColorUtils.of(200, 200, 0))
-//                        .build())
-//                .roundRect(new RoundRectHandler.Builder()
-//                        .arcWidth(20)
-//                        .build())
-//                .getWrapper()
-//                .scale(1.0)
-//                .toFile("F:/4test/imglib/test-out/hash.png");
+        Captors.ofEmpty()
+                .addLast(new HashImageGenerator.Builder("oooo").build())
+//                .addLast(new MonoColorImageGenerator.Builder().width(200).height(200).color(ColorUtils.random()).build())
+                .formatName("png")
+                .toFile("C:\\Users\\Coder\\Desktop\\新建文件夹\\1.png");
 
-
-        Thumbnails.Builder<?> builder = DefaultWrapper
-                .from("F:\\4test\\imglib\\test-out\\before_mosaic.jpeg")
-                .getWrapper()
-                .scale(1.0);
-
-        List<BufferedImage> images = DefaultWrapper.from(builder)
-                .split(new GridSplitStrategy.Builder()
-                        .gridWidth(300)
-                        .gridHeight(100)
-                        .build())
-                .getWrapper()
-                .scale(1.0)
-                .asBufferedImages();
-
-        for (int i = 0; i < images.size(); i++) {
-            ImageIO.write(images.get(i), "PNG", new File("F:\\4test\\imglib\\test-out\\test2\\" + i + ".png"));
-        }
         System.out.println(System.currentTimeMillis() - l1);
-
     }
 
 }
