8a6fdf4a7a9051ccc536e2cafd74ce11af2c7b6b
Ran <ry_always@163.com>
2022-11-18 15:38:35 +0800

    the api of pdf source

--- captor/src/main/java/cn/captor/source/PdfSource.java
+++ captor/src/main/java/cn/captor/source/PdfSource.java
@@ -1,6 +1,9 @@
 package cn.captor.source;
 
 import cn.core.Source;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * PdfSource
@@ -8,5 +11,28 @@ import cn.core.Source;
  * @author tracy
  * @since 1.0.0
  */
-public interface PdfSource<S> extends Source<S, String> {
+public interface PdfSource<S> extends Source<S> {
+
+    /**
+     * 获取最大页码
+     * @return 页码
+     * @throws IOException IO异常
+     */
+    int maxPageNumber() throws IOException;
+
+    /**
+     * PDF 页转图像
+     * @param page 页码
+     * @return 图像
+     * @throws IOException IO异常
+     */
+    BufferedImage read(int page) throws IOException;
+
+    /**
+     * PDF 页转图像列表
+     * @param pages 页码数组
+     * @return 多个图像
+     * @throws IOException IO异常
+     */
+    List<BufferedImage> read(Integer[] pages) throws IOException;
 }
---
+++ captor/src/main/java/cn/captor/source/impl/AbstractPdfBoxSource.java
@@ -0,0 +1,69 @@
+package cn.captor.source.impl;
+
+import cn.captor.source.PdfSource;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * AbstractPdfBoxSource
+ *
+ * @author tracy
+ * @since 1.0.0
+ */
+public abstract class AbstractPdfBoxSource<T> implements PdfSource<T> {
+
+    protected final T source;
+    protected boolean readCompleted = false;
+    protected PDDocument pdf;
+
+
+    public AbstractPdfBoxSource(T source) {
+        this.source = source;
+    }
+
+    @Override
+    public int maxPageNumber() throws IOException {
+        loadIfNot();
+        return pdf.getNumberOfPages();
+    }
+
+    @Override
+    public BufferedImage read(int page) throws IOException {
+        loadIfNot();
+        PDFRenderer renderer = new PDFRenderer(pdf);
+        return renderer.renderImageWithDPI(page + 1, 300, ImageType.RGB);
+    }
+
+    @Override
+    public T getSource() {
+        return source;
+    }
+
+    @Override
+    public List<BufferedImage> read(Integer[] pages) throws IOException {
+        loadIfNot();
+        PDFRenderer renderer = new PDFRenderer(pdf);
+
+        List<BufferedImage> tars = new ArrayList<>();
+        for (Integer page : pages) {
+            tars.add(renderer.renderImageWithDPI(page + 1, 300, ImageType.RGB));
+        }
+        return tars;
+    }
+
+    @Override
+    public boolean isReadCompleted() {
+        return readCompleted;
+    }
+
+    /**
+     * 如果未加载 PDF，则进行加载
+     * @throws IOException IO异常
+     */
+    protected abstract void loadIfNot() throws IOException;
+}
