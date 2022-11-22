f63d1c724ce82e13eaed9fe6107bfe91a2f88007
Ran <ry_always@163.com>
2022-11-22 18:13:27 +0800

    add the module of example

--- example/src/main/java/cn/example/Application.java
+++ example/src/main/java/cn/example/Application.java
@@ -1,13 +1,11 @@
 package cn.example;
 
-import cn.captor.gen.HashImageGenerator;
-import cn.captor.gen.MonoColorImageGenerator;
-import cn.core.utils.ColorUtils;
-import cn.usage.Captors;
-
-import java.awt.*;
+import cn.core.utils.StringUtils;
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+import java.util.Properties;
+import java.util.Scanner;
 
 /**
  * Application
@@ -16,16 +14,73 @@ import java.security.NoSuchAlgorithmException;
  * @since 1.0.0
  */
 public class Application {
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, AWTException {
-        long l1 = System.currentTimeMillis();
 
-        Captors.ofEmpty()
-                .addLast(new HashImageGenerator.Builder("oooo").build())
-//                .addLast(new MonoColorImageGenerator.Builder().width(200).height(200).color(ColorUtils.random()).build())
-                .formatName("png")
-                .toFile("C:\\Users\\Coder\\Desktop\\新建文件夹\\1.png");
+    private static final Properties PROPERTIES = new Properties();
 
-        System.out.println(System.currentTimeMillis() - l1);
+    public static void main(String[] args) {
+        loadConfig("zh");
+        while (true) {
+            printMenus();
+            String type = selectMenu();
+            printFunctions(type);
+            String key = selectFunction();
+            doExample(type, key);
+        }
+    }
+
+    public static void printMenus() {
+        System.out.println("|===========================MENUS===========================|");
+        System.out.println(MessageFormat.format("{0} :::: {1}", "C", PROPERTIES.getProperty("menu.c")));
+        System.out.println(MessageFormat.format("{0} :::: {1}", "Y", PROPERTIES.getProperty("menu.y")));
+        System.out.println(MessageFormat.format("{0} :::: {1}", "T", PROPERTIES.getProperty("menu.t")));
+
+        System.out.print(PROPERTIES.getProperty("menu.tips"));
+    }
+
+    public static String selectMenu() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static void printFunctions(String type) {
+        System.out.println("|=========================FUNCTIONS=========================|");
+        Setting[] values = Setting.values();
+        for (Setting setting : values) {
+            if (setting.type.equals(type)) {
+                String propertyKey = "function." + setting.type.toLowerCase() + "." + setting.key;
+                String desc = PROPERTIES.getProperty(propertyKey);
+                System.out.println(MessageFormat.format("{0} :::: {1}", setting.key, desc));
+            }
+        }
+
+        System.out.print(PROPERTIES.getProperty("function.tips"));
+    }
+
+    public static String selectFunction() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static void doExample(String type, String key) {
+        Setting.Function func = Setting.getFunc(type, key);
+        if (func == null) {
+            System.out.println(PROPERTIES.getOrDefault("unknown.function", "Unknown Command."));
+            return;
+        }
+        func.apply();
+    }
+
+    public static void loadConfig(String language) {
+        try {
+            String proName = StringUtils.isEmpty(language)
+                    ? "menus.properties"
+                    : MessageFormat.format("menus-{0}.properties", language);
+
+            InputStream stream = ClassLoader.getSystemResourceAsStream(proName);
+            PROPERTIES.load(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
---
+++ example/src/main/java/cn/example/CaptorExample.java
@@ -0,0 +1,12 @@
+package cn.example;
+
+/**
+ * CaptorExample
+ *
+ * @author tracy
+ * @since 1.0.0
+ */
+public class CaptorExample {
+    public static void generateTransparentImg() {
+    }
+}
